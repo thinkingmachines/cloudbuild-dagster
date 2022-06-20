4dda0b1cfc945c6552310121f6a1d6b7eeaf8159
jibespanola <tokin21@outlook.com>
2022-06-20 17:15:42 +0800

    override service entrypoints

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 docker-compose up -d --build dagster_postgresql
-docker-compose run --rm dagster_daemon dagster instance migrate
-docker-compose run --rm dagster_daemon dagster schedule up
-docker-compose run --rm dagster_daemon dagster schedule restart --restart-all-running
+docker-compose run --rm --entrypoint dagster dagster_daemon instance migrate
+docker-compose run --rm --entrypoint dagster dagster_daemon schedule up
+docker-compose run --rm --entrypoint dagster dagster_daemon schedule restart --restart-all-running
 docker-compose up -d --build dagit
 docker-compose up -d --build dagster_daemon 
 docker-compose up -d --build dagster_pipelines
