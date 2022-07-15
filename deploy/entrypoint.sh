0ed637a461bdb31c779900e00b635a96aee695f4
jibespanola <tokin21@outlook.com>
2022-07-15 10:40:36 +0800

    added commands for schedules

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 docker-compose up -d --build dagster_postgresql
 docker-compose run --rm --entrypoint dagster dagster_daemon instance migrate
-docker-compose run --rm --entrypoint dagster dagster_daemon schedule up
 docker-compose run --rm --entrypoint dagster dagster_daemon schedule restart --restart-all-running
 docker-compose up -d --build dagit
 docker-compose up -d --build dagster_daemon 
